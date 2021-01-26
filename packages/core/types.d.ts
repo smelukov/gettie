062c467829d62a527af4d11b248f3b34f07948c0
Sergey Melyukov <s.melukov@gmail.com>
2021-01-26 14:48:39 +0300

    (chore): improve types

--- packages/core/types.d.ts
+++ packages/core/types.d.ts
@@ -1,14 +1,40 @@
-declare type Branch = string[];
+declare module '@gettie/core' {
+    export type Branch = string[];
 
-declare type Coverage<T> = {
-  branches: {
-    all: Branch[],
-    used: Branch[],
-    unused: Branch[],
-  },
-  data: {
-    all: T,
-    used: any,
-    unused: any,
-  }
-};
+    export type Coverage<T> = {
+        branches: {
+            all: Branch[],
+            used: Branch[],
+            unused: Branch[],
+        },
+        data: {
+            all: T,
+            used: Partial<T>,
+            unused: Partial<T>,
+        }
+    };
+
+    export class Gettie<T> {
+        static ignoreSymbol: Symbol;
+
+        constructor(data?: T);
+
+        wrapFn<TFn extends (...args: any) => T, TArgs>(fn: TFn, lock: boolean): TFn
+
+        update(data: T, reset: boolean): T
+
+        get(): T | null;
+
+        unwrap(): T | null
+
+        reset(data?: T): void
+
+        lock(): void;
+
+        unlock(): void
+
+        locked(): boolean
+
+        coverage(): Coverage<T>
+    }
+}
